8c8e653a3549ff91642a84c7e5b063eac0b0b1ad
Joel Madrigal <jmadriga@iwu.edu>
2024-02-09 10:13:10 -0600

    got rid of nothing function.

--- app.py
+++ app.py
@@ -23,11 +23,5 @@ def register():
                            wordCount=wordCount, averageWordLength=averageWordLength)
 
 
-@app.route("/")
-def something():
-    return
-
-
-
 if __name__ == '__main__':
     app.run()
