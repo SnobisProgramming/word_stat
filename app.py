19f390d1c3959f6691c5d053500af517f01b082d
Joel Madrigal <jmadriga@iwu.edu>
2024-02-09 09:36:53 -0600

    adjusted the app file.

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():  # put application's code here
+    return render_template("index.html", string=string)
+
+
+@app.route("/wordCount", methods=["POST", "GET"])
+def register():
+    string = request.form.get("string")
+    wordCount = len(string.split())
+
+    totalCharCount = 0
+    for word in string.split(" "):
+        totalCharCount += len(word)
+
+    averageWordLength = totalCharCount / wordCount
+
+    return render_template("wordCount.html", charCount=len(string),
+                           wordCount=wordCount, averageWordLength=averageWordLength)
+
+
+if __name__ == '__main__':
+    app.run()
